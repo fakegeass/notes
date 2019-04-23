592e5c03cb288808a1c2eb7da7afe9d624f4a508
fakegeass <1013002673@qq.com>
2019-04-24 01:56:47 +0800

    redis数据库支持

--- models/notes.go
+++ models/notes.go
@@ -25,6 +25,14 @@ var noteLink map[string]*Note
 
 func init(){
 	noteLink=make(map[string]*Note)
+	if temp,err:=GetNote("","");err==nil{
+		if temp_note,ok:=temp.(map[string]*Note);ok{
+			noteLink=temp_note
+			log.Printf("Read saved note success:%v\n",temp_note["1"])
+		}
+	}else{
+		log.Printf("Read saved note fail for %v\n",err)
+	}
 	log.SetOutput(os.Stdout)
 	log.Println("Notes init...")
 }
@@ -67,7 +75,20 @@ func SetNotes(uuid string,data []byte) error{
 		return err
 	}
 	noteLink[uuid]=temp
+	go saveToRedis(uuid,temp)
 	return nil
+}
+
+func saveToRedis(uuid string,temp *Note){
+	if err:=SaveToRedis(uuid,"Title",temp.Title);err!=nil{
+		log.Println(err)
+	}
+	if err:=SaveToRedis(uuid,"Date",temp.Date);err!=nil{
+		log.Println(err)
+	}
+	if err:=SaveToRedis(uuid,"Data",temp.Data.Val);err!=nil{
+		log.Println(err)
+	}
 }
 
 
@@ -78,8 +99,16 @@ func newUuid()string{
 
 func Delete(uuid string)error{
 	if _,ok:=noteLink[uuid];ok{
+		go deleteForRedis(uuid)
 		delete(noteLink,uuid)
 		return nil
 	}
 	return fmt.Errorf("not exist")
+}
+
+func deleteForRedis(uuid string){
+	err:=deleteInRedis(uuid)
+	if err==nil{
+		log.Printf("Delete %v from redis success.",uuid)
+	}
 }
